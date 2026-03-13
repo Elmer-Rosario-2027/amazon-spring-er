3593f3b79afff6dc8cba1a3aed5899bcde90d710
Elmer Eber Rosario Soriano <eers2015@gmail.com>
2026-03-12 20:20:49 -0500

    NUEVO API FIX

--- src/main/java/org/example/controller/AmazonController.java
+++ src/main/java/org/example/controller/AmazonController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -16,11 +17,15 @@ public class AmazonController {
 
   @GetMapping(value = "/info")
   public ResponseEntity<Object> getInformation(){
-    Map<String, String> responseData = new HashMap<>();
+
+    Map<String, Object> responseData = new HashMap<>();
     responseData.put("status", "200");
     responseData.put("message", "prosecced data success");
-    responseData.put("data", null);
-
+    responseData.put("data", getDtaList());
     return ResponseEntity.ok(responseData);
+  }
+
+  private List<String> getDtaList(){
+    return List.of("elmer", "melissa", "maritza", "clemenacia", "huaraz");
   }
 }
